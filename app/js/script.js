69815b79477ac14294257250ead8d9a58fadd96a
Vasile Midrigan <111rigan111@gmail.com>
2022-05-13 05:13:38 +0100

    build overlay as reaction to hamburger menu button

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,18 @@
-const header = document.querySelector(".header");
-const hamburger = document.getElementById("hamburger");
+"use strict";
 
-hamburger.addEventListener("click", () => {
-  header.classList.toggle("open");
+const btnHamburger = document.getElementById("btn__hamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+header.addEventListener("click", () => {
+  if (header.classList.contains("active")) {
+    header.classList.remove("active");
+    overlay.classList.add("fade-out");
+    overlay.classList.remove("fade-in");
+  } else {
+    header.classList.add("active");
+    overlay.classList.add("fade-in");
+    overlay.classList.remove("fade-out");
+  }
+  console.log(header);
 });
